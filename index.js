276b64a38fa38441ed6d8bf45c34136f453784f0
adityapradhan159 <adityapradhan@tudotechnologies.com>
2024-03-21 16:17:03 +0530

    latesst changes (21.03.24)

--- index.js
+++ index.js
@@ -1,122 +1,3 @@
-// const { MongoClient, ObjectID } = require('mongodb');
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const nodemailer = require('nodemailer');
-// const mongoose = require("mongoose");
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// // MongoDB Connection URL
-// const url = 'mongodb+srv://aditya2test:N0DX5uUKuYZmGuF4@cluster0.mgkp1lm.mongodb.net/';
-// const dbName = 'test'; // Replace with your MongoDB database name
-
-// const uri = `mongodb+srv://aditya2test:N0DX5uUKuYZmGuF4@cluster0.mgkp1lm.mongodb.net/`;
-
-
-// // Connect to MongoDB using Mongoose
-// mongoose.connect(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-//   server.listen(process.env.PORT || 1337, () => {
-//   console.log("webhook is listening");
-// });
-
-// db.once("open", () => {
-//   console.log("Connected to MongoDB");
-
-// })
-
-
-
-
-// // Create a new MongoClient
-// const client = new MongoClient(url);
-
-// // Connect to MongoDB
-// client.connect(function (err) {
-//   if (err) {
-//     console.error('Error connecting to MongoDB', err);
-//     return;
-//   }
-//   console.log('Connected to MongoDB');
-
-//   const db = client.db(dbName);
-//   const collection = db.collection('recipients');
-
-//   // Nodemailer transporter setup
-//   const transporter = nodemailer.createTransport({
-//     /* Your nodemailer transporter configuration here */
-//   });
-
-//   app.post('/sendmail', (req, res) => {
-//     let Sender = req.body['Sender'];
-//     let Recipient = req.body['Recipient'];
-//     let MessageBody = req.body['MessageBody'];
-//     let Subject = req.body['Subject'];
-
-//     let htmlBody = '<p>' + MessageBody + '</p>' + '<img src = "' + Server + '/recipients/' + Recipient + '" style="display:none">';
-//     var mailOptions = {
-//       from: Sender,
-//       to: Recipient,
-//       subject: Subject,
-//       html: htmlBody,
-//     };
-//     transporter.sendMail(mailOptions, function (error, info) {
-//       if (error) {
-//         console.log(error);
-//         res.status(500).send({ error: 'An error occurred while sending the email' });
-//       } else {
-//         // Insert recipient into MongoDB
-//         collection.insertOne({ email: Recipient, opened: false, lastseen: null }, (err, result) => {
-//           if (err) {
-//             console.error('Error inserting recipient into MongoDB', err);
-//             res.status(500).send({ error: 'An error occurred while inserting the recipient into the database' });
-//           } else {
-//             console.log('Recipient inserted into MongoDB');
-//             res.send({ status: 'success' });
-//           }
-//         });
-//       }
-//     });
-//   });
-
-//   app.get('/recipients/:recipient', (req, res) => {
-//     var Recipient = req.params['recipient'];
-//     var date_ob = new Date().toISOString().slice(0, 19).replace('T', ' ');
-
-//     // Update recipient in MongoDB
-//     collection.updateOne({ email: Recipient }, { $set: { opened: true, lastseen: date_ob } }, (err, result) => {
-//       if (err) {
-//         console.error('Error updating recipient in MongoDB', err);
-//         res.status(500).send({ error: 'An error occurred while updating the recipient in the database' });
-//       } else {
-//         console.log('Recipient updated in MongoDB');
-//         res.send({ status: 'success', time: date_ob });
-//       }
-//     });
-//   });
-
-//   // Start the server
-//   const PORT = 3001;
-//   app.listen(PORT, (err) => {
-//     if (err) {
-//       console.error('Error starting the server:', err);
-//     } else {
-//       console.log(`Server is running on port ${PORT}`);
-//     }
-//   });
-
-
-// });
-
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
@@ -126,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://aditya2test:N0DX5uUKuYZmGuF4@cluster0.mgkp1lm.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('your mongodb url', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -149,8 +30,8 @@ const transporter = nodemailer.createTransport({
   /* Your nodemailer transporter configuration here */
   service: "gmail",
   auth: {
-    user: "pradhantestay@gmail.com",
-    pass: "tathkjmyrfgwwxdf",
+    user: "yourEmail",
+    pass: "yourPassword",
   },
 });
 
